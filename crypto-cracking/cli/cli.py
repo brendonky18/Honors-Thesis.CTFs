898ff6dadf334fe8f46d148727bfef4a85ace265
Brendon Ky <brendonky18@gmail.com>
2022-03-04 23:59:30 -0500

    now works
    - correctly typed host parameter
    - checks if connection was made successfully
    - sleeps random number of seconds between messages

--- crypto-cracking/cli/cli.py
+++ crypto-cracking/cli/cli.py
@@ -3,27 +3,36 @@ import argparse
 from ipaddress import ip_address
 from time import sleep
 import signal
+from random import randrange
+
+from sympy import re
 
 # serv = ("10.0.0.115", 0x666c)
 flag = 1
 
-def main(host: int, port: int):
+def main(host: str, port: int):
     print("Starting client")
 
     # connect to server
     serv_sock = socket(AF_INET, SOCK_STREAM)
-    serv_sock.connect((host, port))
-    
+    result = serv_sock.connect_ex((host, port))
+
     def cleanup(*args):
         serv_sock.close()
-        print("\nExiting")
-        exit()
+        print("\nExiting cli")
+        exit(0)
+    
+    if result != 0:
+        print(f"Err: Port {port} on {host} not open")
+        cleanup()
+
 
     signal.signal(signal.SIGINT, cleanup)
     signal.signal(signal.SIGTERM, cleanup)
 
     while True:
         # get the pub key
+        sleep(randrange(10, 20))
         serv_sock.send(b"KEY")
 
         msg = serv_sock.recv(1024)
@@ -36,8 +45,6 @@ def main(host: int, port: int):
         # send messages to server
         serv_sock.send((flag**pub_exponent % mod).to_bytes(1024, "little"))
         print(f"sent {flag**pub_exponent % mod}")
-
-        sleep(20)
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
