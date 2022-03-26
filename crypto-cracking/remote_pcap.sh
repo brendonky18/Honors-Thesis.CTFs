d005e586a6fd24f1eaf904eb8cb64dc748244bd9
Brendon Ky <brendonky18@gmail.com>
2022-03-26 01:48:55 -0400

    fixed ip address

--- crypto-cracking/remote_pcap.sh
+++ crypto-cracking/remote_pcap.sh
@@ -2,4 +2,4 @@
 # USAGE:
 # ./remote_pcap.sh [user_num (0, 1, 2)] [student user name]
 
- ssh -J $2@cs561.cs.umass.edu:56151 user$1@172.30.20.10$1 "cat /tmp/remote_pcap" | wireshark -k -i - 
+ ssh -J $2@cs561.cs.umass.edu:56151 user$1@172.20.30.10$1 "cat /tmp/remote_pcap" | wireshark -k -i - 
