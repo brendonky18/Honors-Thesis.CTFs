d23e6801c8bd6b9ec4b5070faec6beb8cf93048e
Brendon Ky <brendonky18@gmail.com>
2022-03-26 04:33:51 -0400

    can run from terminal

--- crypto-cracking/solutions/fermat.py
+++ crypto-cracking/solutions/fermat.py
@@ -1,11 +1,5 @@
-from math import isqrt, sqrt
-
-
-# n = 715225741 * 982451653 # n = p * q
-
-# a^2 - b&2 = n -> (a+b) and (a-b) are factors of n
-
-
+import argparse
+from math import ceil, isqrt, log
 
 def solve(n: int):
     a = isqrt(n) + 1
@@ -15,3 +9,13 @@ def solve(n: int):
         a += 1
             
     return (a+b, a-b)
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument("encrypted", type=int)
+    p.add_argument("e", type=int)
+    args = p.parse_args()
+
+    decrypted = log(args.e, args.encrypted)
+
+    print(f"Decoded: {int.to_bytes(decrypted, ceil(decrypted.bit_size() / 8), 'big')}")
